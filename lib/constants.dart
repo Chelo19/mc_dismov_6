8ebfbd7e0ea8b75ee53d8628ce3f0e861e999be6
Chelo <marcelodeleongo@gmail.com>
2023-09-27 19:15:38 -0600

    Ya tenemos mapa

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyAraZng-d3dOf2hHTN8NQtwC_uxiv6QxuM";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
